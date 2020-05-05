86afe6515d7c1a2e3299e41529985b3bbfe40a3a
Jonas Almeida Cezar <jcezar.al@gmail.com>
2020-05-05 09:06:19 -0300

    creating process_file

--- app/lambda_function.py
+++ app/lambda_function.py
@@ -1,11 +1,19 @@
+from os import path
 from boto3 import client
-from urllib.parse import unquote_plus
 
 s3_client = client('s3')
 
 
 def lambda_handler(event, context):
-    for record in event['Records']:
-        bucket = record['s3']['bucket']['name']
-        key = unquote_plus(record['s3']['object']['key'])
-        print('bucket:{} key:{}'.format(bucket, key))
+    record = event['Records'][0]
+    bucket = record['s3']['bucket']['name']
+    key = record['s3']['object']['key']
+    print('bucket:{} key:{}'.format(bucket, key))
+
+    input_file = path.join(bucket, key)
+    with s3_client.open(input_file, 'r', newline='', encoding='utf-8-sig') as file:
+        process_file(file)
+
+
+def process_file(file):
+    return None
